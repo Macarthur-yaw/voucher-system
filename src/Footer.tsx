463a868428169a78c3f04d42ad594046f5f406fa
Macarthur-yaw <arthurkevin1260@gmail.com>
2024-04-02 14:04:08 +0100

    done

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
         <div className=" w-fit  mx-auto text-sm mt-10">
-            &copy; 2024 <span className="font-semibold">Group Two(2)</span>.
+            &copy; 2024 <span className="font-semibold">Group Three(3)</span>.
         </div>
      );
 }
